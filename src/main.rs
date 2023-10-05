e789a350ce750a94e6655010b78f199072be71f4
Peter Brzobohaty <petebrzo@gmail.com>
2023-10-05 15:58:01 +0200

    modified hello world.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 use std::io;
-
 fn main() {
-    // Print a message to ask for the user's name
     println!("Enter your name:");
 
-    // Create a new String to store the user input
     let mut name = String::new();
 
-    // Read the user input and store it in the 'name' variable
     io::stdin()
         .read_line(&mut name)
-        .expect("Failed to read line");
+        .expect("Failed to read line.");
 
-    // Trim the trailing newline character from the input
     let name = name.trim();
 
-    // Print a greeting message with the user's name
-    println!("Hello, {}!", name);
+    println!("How are you doing {}?", name);
+
+    let mut mood = String::new();
+    io::stdin()
+        .read_line(&mut mood)
+        .expect("Failed to read line.");
+
+    let mood = mood.trim().to_lowercase();
+
+    if mood.eq("great") || mood.eq("cool") {
+        println!("That is great {}!", name);
+    } else {
+        println!("{}, I hope you will get better.", name);
+    }
 }
