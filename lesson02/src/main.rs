1ff6a6bea9cb98705733dce5aa4265c855100cc4
Peter Brzobohaty <petebrzo@gmail.com>
2023-10-16 14:59:09 +0200

    Lesson02 added transmutations.

--- lesson02/src/main.rs
+++ lesson02/src/main.rs
@@ -1,4 +1,5 @@
 // two own transmutations
+use convert_case::{Case, Casing};
 use slug::slugify;
 use std::{env, io};
 
@@ -12,23 +13,15 @@ fn main() {
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line.");
+    let input = input.trim();
 
-    println!("Content of args: {:?}", args);
-
-    //     if args[1] == "lowercase" {
-    //         println!("{}", input.to_lowercase());
-    //     } else if args[1] == "uppercase" {
-    //         println!("{}", input.to_uppercase());
-    //     } else if args[1] == "no-spaces" {
-    //         println!("{}", input.replace(" ", ""));
-    //     } else if args[1] == "slugify" {
-    //         println!("{}", slugify(&input));
-    //     }
-    match args[1].as_str() {
+    match args.get(1).unwrap().as_str() {
         "lowercase" => println!("{}", input.to_lowercase()),
         "uppercase" => println!("{}", input.to_uppercase()),
         "replace" => println!("{}", input.replace(" ", "")),
-        "slugify" => println!("{}", slugify(&input)),
-        _ => println!("Valid input parameter missing."),
+        "slugify" => println!("{}", slugify(input)),
+        "snake" => println!("{}", input.to_case(Case::Snake)),
+        "camel" => println!("{}", input.to_case(Case::Camel)),
+        _ => println!("Invalid parameter.",), // "camel" => println!("{}", input.to_case)
     }
 }
